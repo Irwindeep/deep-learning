f8a7fc68add6a627ae0ebb256090aa59a8cbaee9
Irwindeep <b22ai022@iitj.ac.in>
2024-12-03 14:45:20 +0530

    Defined variable

---
+++ dl/variable.py
@@ -0,0 +1,10 @@
+import numpy as np
+from dl.tensor import Tensor
+
+class Variable(Tensor):
+    def __init__(self, *shape, random_seed: int = 20) -> None:
+        self.random_seed = random_seed
+        np.random.seed(self.random_seed)
+
+        data = np.random.randn(*shape)
+        super().__init__(data, requires_grad=True)
