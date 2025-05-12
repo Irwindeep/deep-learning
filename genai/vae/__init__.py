ee0c419111dc06d33f3fb335d74058215c784833
Irwindeep <b22ai022@iitj.ac.in>
2025-05-12 20:24:19 +0530

    Anime Face dataset for VAE

--- genai/vae/__init__.py
+++ genai/vae/__init__.py
@@ -1,8 +1,9 @@
-from .dataset import get_datasets, get_loaders, get_transform
+from .dataset import AnimeFaceDataset, get_datasets, get_loaders, get_transform
 from .model import VAE
 from .utils import ConvBlock, UpsampleBlock
 
 __all__ = [
+    "AnimeFaceDataset",
     "ConvBlock",
     "get_datasets",
     "get_loaders",
--- genai/vae/dataset.py
+++ genai/vae/dataset.py
@@ -1,9 +1,32 @@
-from typing import Callable, Tuple
+import os
+from typing import Any, Callable, Optional, Tuple
 
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset, random_split
-from torchvision.datasets import CelebA
 from torchvision.transforms import transforms
+
+
+# kaggle dataset - https://www.kaggle.com/datasets/splcher/animefacedataset
+class AnimeFaceDataset(Dataset):
+    def __init__(self, root: str, transform: Optional[Callable]) -> None:
+        super(AnimeFaceDataset, self).__init__()
+
+        self.root = (root,)
+        self.image_files = os.listdir(root)
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return len(self.image_files)
+
+    def __getitem__(self, index: int) -> Any:
+        image_file = self.image_files[index]
+        image = Image.open(image_file)
+
+        if self.transform:
+            return self.transform(image)
+
+        return image
 
 
 def get_transform() -> Tuple[Callable, Callable]:
@@ -20,31 +43,23 @@ def get_transform() -> Tuple[Callable, Callable]:
     return transform, inv_norm
 
 
-def get_datasets(transform: Callable, seed: int = 12) -> Tuple[Dataset, ...]:
+def get_datasets(root: str, transform: Callable, seed: int = 12) -> Tuple[Dataset, ...]:
     torch.manual_seed(seed)
 
-    train_dataset = CelebA(
-        root="celeba", split="train", transform=transform, download=True
-    )
-    test_dataset = CelebA(
-        root="celeba", split="test", transform=transform, download=False
-    )
+    dataset = AnimeFaceDataset(root=root, transform=transform)
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
 
-    train_size = int(0.8 * len(train_dataset))
-    val_size = len(train_dataset) - train_size
-
-    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
-
-    return train_dataset, val_dataset, test_dataset
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    return train_dataset, val_dataset
 
 
 def get_loaders(
     *datasets: Dataset, batch_size: int
 ) -> Tuple[DataLoader[torch.Tensor], ...]:
-    train_dataset, val_dataset, test_dataset = datasets
+    train_dataset, val_dataset = datasets
 
     train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
 
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader
